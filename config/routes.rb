27f2a9b654efb5a6587e8cd873a070b2d314760e
kellylog <kellylogblog@gmail.com>
2016-04-25 12:55:38 +0000

    lesson 17

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Flixter::Application.routes.draw do
   resources :courses, only: [:index, :show]
 
   namespace :instructor do
-     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create] do
-        resources :lessons, only: [:new, :create]
+      resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
     end
-    
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -66,5 +67,4 @@ Flixter::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
 end
